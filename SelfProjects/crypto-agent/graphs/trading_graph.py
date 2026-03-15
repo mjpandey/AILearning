448ddb0d544b32287f3532a6e682ecdaf66cda3d
siddharg <siddharg@adobe.com>
2026-03-15 14:15:14 +0530

    added system prompt

--- SelfProjects/crypto-agent/graphs/trading_graph.py
+++ SelfProjects/crypto-agent/graphs/trading_graph.py
@@ -1,15 +1,22 @@
 from langgraph.graph import StateGraph
-from agents.trading_agent import agent
+from agents.trading_agent import agent, system_prompt
 from graphs.state import AgentState
 
 
-def analyze_query(state: AgentState):
-    result = agent.run(state["query"])
+def analyze_queryAgent(state: AgentState):
+
+    query = state["query"]
+    print("System Prompt:\n", system_prompt)
+    print("User Query:\n", query)   
+
+    analyze_query = "\n system_prompt:\n"  + system_prompt + "\n\nUser Query:\n" + query
+
+    result = agent.run(analyze_query)
     return {"result": result}
 
 graph = StateGraph(AgentState)
 
-graph.add_node("analyze", analyze_query)
+graph.add_node("analyze", analyze_queryAgent)
 
 graph.set_entry_point("analyze")
 
